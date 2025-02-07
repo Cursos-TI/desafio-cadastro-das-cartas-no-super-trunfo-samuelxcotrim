868da41388207b293b29bad294662e154b6d7e76
samuelxcotrim <royalewar94@gmail.com>
2025-02-07 18:12:31 +0000

    Estou Cadastrando as cidades e estados do pais

--- DesafioCartas.c
+++ DesafioCartas.c
@@ -1,10 +1,63 @@
 #include <stdio.h>
 
-main(){
-  printf("Desafio Cartas Super Trunfo");
-
-
-
-
-
+//Concerteza alguns PIB das cidades não estão corretos, por conta de não conseguir achar informção exata deles, Quase todos nem mostram em sites ou em chatgpt. Por isso coloquei o PIB do estado e não da cidade.
+int main(){
+  char pais [20] = "Argentina";
+  char Estado1 [20] = "buenos Aires";
+  char P01 [20] = "palermo";
+  double populacao_p01 = 252.312;
+  float area_p01 = 15.7;
+  double pib_p01 = 119.651;
+  int pontos_p01 = 13;
+  char R02 [20] = "recoleta";
+  double populacao_r02 = 188.780;
+  float area_r02 = 5.5;
+  double pib_r02 = 119.651;
+  int pontos_r02 = 13;
+  char V03 [20] = "vicente lopez";
+  double populacao_v03 = 282.281;
+  float area_v03 = 34.5;
+  double pib_v03 = 119.651;
+  char T04 [20] = "tandil";
+  double populacao_t04 = 150.162
+  float area_t04 = 3.8
+  char Estado2 [20] = "chaco";
+  char S01 [20] = "san bernado";
+  double populacao_s01 = 1142.963
+  float area_s01 = 99.63
+  double pib_s01 = 
+  char R02 [20] = "resistencia";
+  char C03 [20] = "corzuela";
+  char C04 [20] = "charata";
+  char Estado3 [20] = "entre Rios";
+  char P01 [20] = "parana";
+  char D02 [20] = "diamante";
+  char V03 [20] = "victoria";
+  char V04 [20] = "villaguay";
+  char Estado4 [20] = "misiones";
+  char P01 [20] = "posadas";
+  char S02 [20] = "san ignacio";
+  char S03 [20] = "san pedro";
+  char E04 [20] = "eldorado";
+  char Estado5 [20] = "terra do fogo";
+  char U01 [20] = "ushuaia";
+  char P02 [20] = "puerto williams";
+  char P03 [20] = "porvenir";
+  char R04 [20] = "rio grande";
+  char Estado6 [20] = "cordova";
+  char C01 [20] = "cordoba";
+  char D02 [20] = "dean funes";
+  char C03 [20] = "cruz del eje";
+  char V04 [20] = "villa dollores";
+  char Estado7 [20] = "santa fe";
+  char V01 [20] = "venado tuerto";
+  char T02 [20] = "tostado";
+  char A03 [20] = "armstrong";
+  char L04 [20] = "las toscas";
+  char Estado8 [20] = "la pampa";
+  char G01 [20] = "general pico";
+  char B02 [20] = "bernasconi";
+  char A03 [20] = "arata";
+  char A04 [20] = "anguil";
+  
 }
